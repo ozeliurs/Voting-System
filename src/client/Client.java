e26b4ed267c65de1f6181612f14d65409173eace
Timothee_Juillet <astagios01@gmail.com>
2023-10-02 10:19:44 +0200

    feat: add client main

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,7 @@
 package client;
 
+import server.Distant;
+
 import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -8,5 +10,9 @@ import java.rmi.RemoteException;
 
 public class Client implements Serializable {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
+        Distant distant = (Distant) Naming.lookup("rmi://localhost:2001/objDist");
+        System.out.println("Avant echo\n");
+        distant.echo();
+        System.out.println("Apres echo\n");
     }
 }
