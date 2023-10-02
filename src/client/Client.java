5a6c73ca8df00753d33f381025ecd56f01db2dc5
Ozeliurs <ozeliurs@gmail.com>
2023-10-02 11:41:25 +0200

    feat: add password encryption

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import exceptions.BadCredentialsException;
 import shared.UserRepository;
 
 import java.io.Serializable;
@@ -10,9 +11,32 @@ import java.rmi.RemoteException;
 
 public class Client implements Serializable {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
+        // Load remote objects
         UserRepository userRepository = (UserRepository) Naming.lookup("rmi://localhost:2001/userRepository");
 
-        userRepository.checkCredentials("123456", "123456");
+        // Login
+        System.out.println("=== Login ===");
 
+        System.out.print("login: ");
+        String login = System.console().readLine();
+
+        System.out.print("password: ");
+        String password = System.console().readLine();
+
+        // Hash password
+        SCryptPasswordEncoder encoder = new SCryptPasswordEncoder();
+        password = encoder.encode(password);
+
+        try {
+            userRepository.checkCredentials(login, password);
+            System.out.println("=== Logged in ===");
+        } catch (BadCredentialsException e) {
+            System.out.println("=== Bad credentials ===");
+            return;
+        }
+
+        // Vote
+        System.out.println("=== Vote ===");
+        System.out.println("TODO");
     }
 }
