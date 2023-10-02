d4fef7ac917eb16400dfc9da413130bf08af33c2
Ozeliurs <ozeliurs@gmail.com>
2023-10-02 11:03:51 +0200

    feat: add exceptions

---
+++ src/exceptions/BadCredentialsException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class BadCredentialsException extends Exception {
+
+}
---
+++ src/exceptions/HasAlreadyVotedException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class HasAlreadyVotedException extends Exception {
+}
